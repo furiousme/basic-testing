3a8d96598deded275a6c88664476240840b9710b
furiousme <shuptaroks@gmail.com>
2024-10-23 14:58:40 +0300

    test: add tests for 07 module

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,55 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('lodash', () => ({
+  throttle: jest.fn((a) => a),
+}));
 
 describe('throttledGetDataFromApi', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValueOnce({ data: {} });
+    const createSpy = jest
+      .spyOn(axios, 'create')
+      .mockImplementationOnce(
+        () => ({ get: mockGet } as unknown as AxiosInstance),
+      );
+
+    await throttledGetDataFromApi('/api');
+    expect(createSpy).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValueOnce({ data: {} });
+    jest
+      .spyOn(axios, 'create')
+      .mockImplementationOnce(
+        () => ({ get: mockGet } as unknown as AxiosInstance),
+      );
+
+    await throttledGetDataFromApi('/api');
+    expect(mockGet).toHaveBeenCalledWith('/api');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const responseData = "'response data";
+    const mockGet = jest.fn().mockResolvedValueOnce({ data: responseData });
+    jest
+      .spyOn(axios, 'create')
+      .mockImplementationOnce(
+        () => ({ get: mockGet } as unknown as AxiosInstance),
+      );
+
+    const response = await throttledGetDataFromApi('/api');
+    expect(response).toBe(responseData);
   });
 });
